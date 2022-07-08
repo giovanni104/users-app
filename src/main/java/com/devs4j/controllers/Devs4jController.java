f3b375aa8f5402a5c7d0348ebd4a871265536105
giovanni104 <giovanni104@hotmail.com>
2022-07-07 23:17:13 -0500

    servicios rest users

--- src/main/java/com/devs4j/controllers/Devs4jController.java
+++ src/main/java/com/devs4j/controllers/Devs4jController.java
@@ -1,5 +1,18 @@
 package com.devs4j.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class Devs4jController {
 
+	@GetMapping
+	public String helloWord() {
+		return "hello word";
+	}
+	
+	
+	
 }
